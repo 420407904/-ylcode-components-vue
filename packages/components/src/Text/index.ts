f2099986b1b2e88f0801d0e3c0f17409c5707cec
yangliang <yangliang@xizhi-ai.com>
2023-03-01 16:56:21 +0800

    初始化项目

--- packages/components/src/Text/index.ts
+++ packages/components/src/Text/index.ts
@@ -1,4 +1,4 @@
 import Text from './index.vue';
-import { withInstall } from '@kitty-plus/utils';
+import { withInstall } from '@ylcode/utils';
 const MedexText = withInstall(Text);
 export default MedexText;
